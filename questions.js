4162de4ffad8c9ffc0b1064489a08e868de96c21
Ayush Agrawal <ayushagrawal1811@gmail.com>
2025-05-31 16:31:18 +0530

    Added questions related to arts and literature

---
+++ questions.js
@@ -0,0 +1,103 @@
+let arts_and_literature = [
+    {
+        question: "Which author wrote 'The Return of Sherlock Holmes'?",
+        options: ["Agatha Christie", "Oscar Wilde", "George Orwell", "Arthur Conan Doyle"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'A Scandal in Bohemia'?",
+        options: ["Agatha Christie", "Oscar Wilde", "George Orwell", "Arthur Conan Doyle"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'A Hero of Our Time'?",
+        options: ["Nikolai Gogol", "Mikhail Bulgakov", "Alexander Pushkin", "Mikhail Lermontov"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the surname of Cathy in \"Wuthering Heights\"?",
+        options: ["Heathcliffe", "Darcy", "Bingby", "Earnshaw"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which book contains the character 'Meursault'?",
+        options: ["One Hundred Years of Solitude", "The Trial", "The Quiet American", "The Stranger"],
+        correctAnswer: 3
+    },
+    {
+        question: "What did Dorothy and her friends walk down in The Wizard of Oz?",
+        options: ["The Red Brick Road", "The Emerald Road", "The Sapphire Road", "The Yellow Brick Road"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'Annabel Lee'?",
+        options: ["Stephen King", "Nathaniel Hawthorne", "Mario Puzo", "Edgar Allan Poe"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which book contains the character 'Florentino Ariza'?",
+        options: ["Portnoy's Complaint", "Pale Fire", "Tinker", "Love in the Time of Cholera"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which book contains the character 'Leopold Bloom'?",
+        options: ["We Have Always Lived in the Castle", "The Quiet American", "The Sound and the Fury", "Ulysses"],
+        correctAnswer: 3
+    },
+    {
+        question: "In an early Stephen King novel, which titular character is a student at Ewen High?",
+        options: ["Annie", "Megan", "Emily", "Carrie"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which book contains the character 'Stephen Maturin'?",
+        options: ["Gone With the Wind", "Heart of Darkness", "The Thin Man", "Master and Commander"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'The Legend of the Condor Heroes'?",
+        options: ["C. S. Lewis", "Edgar Rice Burroughs", "J. K. Rowling", "Jin Yong"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'Kafka on the Shore'?",
+        options: ["Oscar Wilde", "Umberto Eco", "Søren Kierkegaard", "Haruki Murakami"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the name of the miserly main character in A Christmas Carol?",
+        options: ["Charles Dickens", "Bob Cratchit", "Uriah Heep", "Ebenezer Scrooge"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'New Moon'?",
+        options: ["Jack Vance", "Brandon Sanderson", "Robert A. Heinlein", "Stephenie Meyer"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'Mortal Engines'?",
+        options: ["Oscar Wilde", "Robert Louis Stevenson", "Christopher Tolkien", "Philip Reeve"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'Harry Potter and the Half-Blood Prince'?",
+        options: ["Christopher Tolkien", "Philip Pullman", "C. S. Lewis", "J. K. Rowling"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which artist painted \"The Grand Odalisque\"",
+        options: ["Caravaggio", "Jackson Pollock", "El Greco", "Jean Auguste Dominique Ingres"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'Skeleton Crew'?",
+        options: ["H. P. Lovecraft", "Edgar Allan Poe", "Jack Vance", "Stephen King"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which author wrote 'The Dispossessed'?",
+        options: ["Stephen King", "Jack Vance", "George R. R. Martin", "Ursula K. Le Guin"],
+        correctAnswer: 3
+    },
+]
+
