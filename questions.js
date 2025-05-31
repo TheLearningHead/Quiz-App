bcb448512dfaacab7b42b5ece681f0f19cf36550
Ayush Agrawal <ayushagrawal1811@gmail.com>
2025-05-31 16:34:55 +0530

    Added questions related films and tv

--- questions.js
+++ questions.js
@@ -101,3 +101,105 @@ let arts_and_literature = [
     },
 ]
 
+let film_and_tv = [
+    {
+        question: "Which film features the quote 'Good morning! And in case I don't see you, good afternoon, good evening, and good night!'?",
+        options: ["Eternal Sunshine of the Spotless Mind", "Dead Poet's Society", "The Shawshank Redemption", "The Truman Show"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of these quotes is from the film 'No Country for Old Men'?",
+        options: ["My precious.", "I'll be back.", "Very nice!", "What’s the most you ever lost on a coin toss?"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of these quotes is from the film 'The Graduate'?",
+        options: ["Hello, gorgeous.", "Fasten your seatbelts. It's going to be a bumpy night.", "We’re goin’ streaking!", "Plastics."],
+        correctAnswer: 3
+    },
+    {
+        question: "Terminator 2: Judgment Day was released in which year?",
+        options: ["1987", "1995", "1999", "1991"],
+        correctAnswer: 3
+    },
+    {
+        question: "Name the movie that matches the following plot summary: 'Violence and mayhem ensue after a man stumbles upon more than two million dollars.'",
+        options: ["Hacksaw Ridge", "Pulp Fiction", "Aliens", "No Country for Old Men"],
+        correctAnswer: 3
+    },
+    {
+        question: "Who played the role of James Bond in You Only Live Twice?",
+        options: ["Timothy Dalton", "Daniel Craig", "George Lazenby", "Sean Connery"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the plot of the movie Logan?",
+        options: ["Born in the stable next door to Christ, a man spends his life being mistaken for a messiah.", "A law enforcement agent pursues four replicants who have returned to Earth to find their creator.", "A boy and his little sister struggle to survive in Japan during World War II.", "A weary mutant leads a quiet life until a mutant child pursued by scientists comes to him for help."],
+        correctAnswer: 3
+    },
+    {
+        question: "Name the movie that matches the following plot summary: 'When a heist goes wrong, the surviving criminals suspect that one of them is a police informant.'",
+        options: ["Saving Private Ryan", "The Green Mile", "Gran Torino", "Reservoir Dogs"],
+        correctAnswer: 3
+    },
+    {
+        question: "Name the movie that matches the following plot summary: 'A girl with her own sense of justice decides to help those around her and discovers love.'",
+        options: ["Raging Bull", "The Matrix", "Warrior", "Amélie"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which actor has starred in both The Amazing Spider-Man 2 and Django Unchained?",
+        options: ["Hugo Weaving", "Warwick Davis", "Timothy Spall", "Jamie Foxx"],
+        correctAnswer: 3
+    },
+    {
+        question: "What TV game show had a 'Phone-a-Friend' lifeline?",
+        options: ["Jeopardy!", "Wheel of Fortune", "The Price Is Right", "Who Wants To Be A Millionaire?"],
+        correctAnswer: 3
+    },
+    {
+        question: "In which year was Fargo first released in the cinema?",
+        options: ["1992", "2000", "2004", "1996"],
+        correctAnswer: 3
+    },
+    {
+        question: "Released in 2022, which TV show depicts a world where people undergo a procedure to split their mind into two so that they can enjoy life without work?",
+        options: ["Squid Game", "Split", "Mr Robot", "Severance"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of these quotes is from the film 'Titanic'?",
+        options: ["That is so fetch.", "This is the day you will always remember as the day you almost caught Captain Jack Sparrow!", "Wait a minute, wait a minute. You ain't heard nothin' yet!", "I'm the king of the world!"],
+        correctAnswer: 3
+    },
+    {
+        question: "Name the movie that matches the following plot summary: 'The daughter of a plantation owner conducts a romance with a roguish profiteer.'",
+        options: ["Downfall", "My Neighbour Totoro", "Up", "Gone with the Wind"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the plot of the movie Inside Out?",
+        options: ["A slacker seeks restitution for a rug ruined by debt collectors.", "POWs are forced to build a railway, not knowing that allies are planning to destroy it.", "In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler.", "After a girl is uprooted from her life, her emotions conflict on how best to navigate a new life."],
+        correctAnswer: 3
+    },
+    {
+        question: "Which actor has featured in films including Arrested Development and Donnie Darko?",
+        options: ["Russell Crowe", "Jean-Claude Van Damme", "Pierce Brosnan", "Seth Rogen"],
+        correctAnswer: 3
+    },
+    {
+        question: "Name the movie that matches the following plot summary: 'After awakening from a coma, a former assassin wreaks vengeance on those who betrayed her.'",
+        options: ["Million Dollar Baby", "Unforgiven", "Dead Poets Society", "Kill Bill: Vol. 1"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which director directed Jurassic Park?",
+        options: ["Woody Allen", "Martin Scorsese", "Alfred Hitchcock", "Steven Spielberg"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which actress has appeared in films including 'Requiem for a Dream' in 2000 and 'Hulk' in 2003?",
+        options: ["Rachel Weisz", "Frances Fisher", "Salma Hayek", "Jennifer Connelly"],
+        correctAnswer: 3
+    },
+]
